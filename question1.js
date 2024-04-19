965e6af6d2e060d6c8b8867959b7c780f761ffbb
AndrewOhwoka <andrew.ohwoka@keyin.com>
2024-04-19 10:23:12 -0230

    Commit6

--- question1.js
+++ question1.js
@@ -1,48 +1,51 @@
 class User {
-    constructor(n, a) {
-      this.name = n;
-      this.age = a;
-    }
+  constructor(n, a) {
+    this.name = n;
+    this.age = a;
+  }
 }
 
 function clearFields() {
-    document.querySelector("#name").value = "";
-    document.querySelector("#age").value = "";
+  document.querySelector("#name").value = "";
+  document.querySelector("#age").value = "";
 }
 
 let user; // Declare user outside the event listeners so it can be accessed in both
 
 window.addEventListener("DOMContentLoaded", function (e) {
-    document.querySelector("#make").addEventListener("click", function (e) {
-        // Code to be executed when the button is clicked
-        let name = document.querySelector("#name").value;
-        let age = document.querySelector("#age").value.trim();
-        age = parseInt(age); //convert to number
-        if (!isNaN(age)) {
-            console.log("Age is a number");
-        } else {
-           alert("Age must be a number");
-           return;
-        }
-        user = new User(name, age);
-        if (user.name === "" || isNaN(user.age)) {
-            alert("Please enter a name and age");
-            return;
-        } 
-        console.log(user);
-        clearFields(); // Clear the fields after creating the user
-        e.preventDefault();
-    });
+  document.querySelector("#make").addEventListener("click", function (e) {
+    // Code to be executed when the button is clicked
+    let name = document.querySelector("#name").value;
+    let age = document.querySelector("#age").value.trim();
+    user = new User(name, age);
+    if (user.name === "" || user.age === "") {
+      alert("Please enter a Name and Age");
+      return;
+    }
+  
+    age = parseInt(age); //convert to number
+    if (!isNaN(age)) {
+      console.log("Age is a number");
+    } else {
+      alert("Age must be a number");
+      return;
+    }
+    console.log(user);
+    clearFields(); // Clear the fields after creating the user
+    e.preventDefault();
+  });
 
-    document.querySelector("#displayButton").addEventListener("click", function (e) {
-        // Code to be executed when the button is clicked
-        if (user) {
-            let html = `<p> Name: ${user.name} <br/> Age: ${user.age}</p> `;
-            document.querySelector("#userInfo").innerHTML = html;
-            clearFields(); // Clear the fields after displaying the user
-        } else {
-            console.log("No user to display");
-        }
-        e.preventDefault();
+  document
+    .querySelector("#displayButton")
+    .addEventListener("click", function (e) {
+      // Code to be executed when the button is clicked
+      if (user) {
+        let html = `<p> Name: ${user.name} <br/> Age: ${user.age}</p> `;
+        document.querySelector("#userInfo").innerHTML = html;
+        clearFields(); // Clear the fields after displaying the user
+      } else {
+        console.log("No user to display");
+      }
+      e.preventDefault();
     });
 });
